bebe87dcef08d1610dcc733b1e3750edeb3d2eb8
patrick-faulkner <45801641+patrick-faulkner@users.noreply.github.com>
2020-03-02 16:22:27 -0600

    Python Testing
    explanation of Python object use, and lists, tuples, dictionaries, etc.

--- pythonTest.py
+++ pythonTest.py
@@ -16,11 +16,58 @@ def main():
 	# a new string object created and bound to some_guy
 	some_guy = "Bill"
 
-	# tuples are immutable lists, declared w/ parenthesis 
-	name_tuple = (first_names, another_list_of_names)
-
-	print(some_guy, first_names, another_list_of_names, name_tuple)	
+	print("List: ", some_guy, first_names, another_list_of_names, "\n")	
 	# outputs Bill, ['Fred', 'George'] ['Fred', 'George']
 	# this is because first_names and another_list_of_names are bound to the same list object
+######################################################################################################################################################################## tuples
+
+	# tuples are immutable lists, declared w/ parenthesis 
+	# can be accessed like list
+	another_name = "Todd"
+	name_tuple = (some_guy, another_name)
+	print("Tuple: ", name_tuple, name_tuple[0], "\n")
+######################################################################################################################################################################## sets
+
+	# sets: unordered unindexed list, cannot access individual index usint set[i]
+	# declared w/ curly brace
+	# cannot chnage objects in set once declared but can add objects
+	name_set = {some_guy, another_name}
+	print("Set: ", name_set)
+	for x in name_set:
+		print(x) 
+	print("\nFred in set: ", "Fred" in name_set)
+	name_set.add("Hank")
+	print("Hank in set: ", "Hank" in name_set)
+	name_set.update(["Will", "Olive", "Cheese"])
+	print("\nUpdated Set: ")
+	for x in name_set:
+		print(x)
+	print("\nLength of set: ", len(name_set))
+	name_set.remove("Cheese")
+	print("Length of set after removal: ", len(name_set))
+	# set.remove() will raise error if object doesnt exist, can use set.discard() and will not raise error if object not in set
+	# can use pop() to remove last object, but because of nature of set, will be random
+	# clear() clears content of set, del() deletes set completely
+	# set3 = set1.union(set2) creates new set w/ content of both sets
+
+######################################################################################################################################################################## dictionaries
+	# disctionaries: unordered, changable and indexed
+	# has keys and values
+	testDic = {
+		"name": "Patrick",
+		"age": 24,
+		"major": "compsci"
+	}
+	print("\nDictionary: ", testDic)
+	# access values --> use keys
+	x = testDic["age"]
+	print("Age: ", x)
+	x = testDic.get("name")
+	testDic["name"] = "Haquah"
+	print("Name: ", x, "\n")
+	for x, y in testDic.items():
+		print(x, ": ", y)
+
+
 
 main()
